d068d3e9686c112a62e5b98d9e736db709781fa4
yunlong <yunlong@baidu.com>
2014-03-06 14:34:07 +0800

    Initial Commit

---
+++ openglE/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  openglE
+//
+//  Created by yun long on 14-3-6.
+//  Copyright (c) 2014年 yun long. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
